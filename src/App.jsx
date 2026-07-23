81bdef08d64cfb6f249e5048e9c5e3d8c2d12f42
Kaif <kaif.sheikh.0820@gmail.com>
2026-07-23 15:52:05 +0500

    Add components and section in main.jsx and add styles in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import Navbar from "./components/Navbar"
+import Hero from "./section/Hero"
+import About from "./section/About"
+import Project from "./section/Project"
+import Testimonials from "./section/Testimonials"
+import Contact from "./section/Contact"
+import Footer from "./components/Footer"
 
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Project />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
